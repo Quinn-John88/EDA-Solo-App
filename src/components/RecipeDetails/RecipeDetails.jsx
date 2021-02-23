e4be9078d68204f6952ea0288354d751b4baccc4
Quinn Johnson <johnson.fargo87@gmail.com>
2021-02-22 22:59:36 -0600

    added details page still not working properly

---
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -0,0 +1,24 @@
+import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+
+
+function MovieDetails() {
+
+    const history = useHistory();
+    const recipe = useSelector(store => store.recipes);
+    console.log(recipe.id);
+    return (
+        <>
+            <button onClick={() => history.push('/recipes')}>Back to List</button>
+            <div key={recipe.id} >
+                <h3>{recipe.title}</h3>
+                <h2>Ingredients</h2>
+                    <p>{recipe.ingredients_list}</p>
+                <h2>Cooking Instrucitons</h2>
+                    <p>{recipe.cooking_directions}</p>
+            </div>
+        </>
+    )
+}
+
+export default MovieDetails; 
