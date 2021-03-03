6d4b5e354defff467f710e9210cabbc224c9de17
Quinn Johnson <johnson.fargo87@gmail.com>
2021-03-02 19:47:28 -0600

    changed route for recipes to be user specific

--- server/routes/recipe.router.js
+++ server/routes/recipe.router.js
@@ -1,20 +1,21 @@
 const express = require('express');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const pool = require('../modules/pool');
 const router = express.Router();
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
     const query=`
     SELECT * FROM "recipes"
     WHERE "recipes"."id" IN (SELECT DISTINCT "recipe_ingredients"."recipe_id" FROM "ingredient_user"
     RIGHT JOIN "recipe_ingredients" ON "ingredient_user"."ingredient_id"="recipe_ingredients"."ingredient_id"
-    WHERE "recipe_ingredients"."recipe_id" 
+    WHERE "ingredient_user"."user_id"=$1 AND "recipe_ingredients"."recipe_id"
     NOT IN (SELECT DISTINCT "recipe_ingredients"."recipe_id" FROM "ingredient_user"
     RIGHT JOIN "recipe_ingredients" ON "ingredient_user"."ingredient_id"="recipe_ingredients"."ingredient_id"
     WHERE "ingredient_user"."ingredient_id" IS NULL));`
-    pool.query(query).then(result => {
+    pool.query(query,[req.user.id]).then(result => {
         res.send(result.rows)
     }).catch(err => {
         res.sendStatus(500)
